6a17da0c51424bc6de52c48101cbaace179cacd1
Javed Ali <Javedali90045@gmail.com>
2024-10-22 00:08:57 +0530

    UI and logic Building done

--- src/Components/LoadMore.jsx
+++ src/Components/LoadMore.jsx
@@ -1,38 +1,64 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import './LoadMore.css'
 
 const LoadMore = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disable, setDisable] = useState(false);
 
-    const [loading, setLoading] = useState(false)
-    const [products, setProducts] = useState([])
-    const [count, setCount] = useState(0)
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
 
-   async function fetchProducts()
-   {
-    try{
-            setLoading(true)
-            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${
-                count === 0 ? 0 : count * 20}`)
-            const data = await response.json();
-            console.log(data)
-            setProducts(data)
-            setLoading(false)
-    }catch(e){
-        setError(e)
-        setLoading(false)
+      const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+    } catch (e) {
+      setError(e);
+      setLoading(false);
     }
-   }
+  }
 
-   useEffect(()=>{
-    fetchProducts()
-   }, [])
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
 
-   if(loading)
-   {
+  useEffect(() => {
+    if (products && products.length === 100) setDisable(true);
+  }, [products]);
+
+  if (loading) {
     return <div>Loading data ! Please wait.</div>;
-   }
+  }
   return (
-    <div>LoadMore</div>
-  )
-}
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button disabled={disable} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+        {disable ? <p>You have reached to 100 products</p> : null}
+      </div>
+    </div>
+  );
+};
 
-export default LoadMore
+export default LoadMore;
